3b15540058464bf47476271452d012d8a8f4c8fd
jforster <jforster>
2015-11-04 11:44:15 +0000

    Added missing address entity

--- src/main/java/com/qa/James/entities/Address.scala
+++ src/main/java/com/qa/James/entities/Address.scala
@@ -3,6 +3,6 @@ package com.qa.James.entities
 /**
  * @author jforster
  */
-class Address {
+class Address (val idAddress:Int, val idCustomer:Int, val addressLines:Array[String], val city:String, val postcode:String){
   
 }
