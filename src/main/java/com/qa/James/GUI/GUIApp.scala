64b685ea72519d5fd9ca8e26350cf616d71ddef5
jforster <jforster>
2015-11-05 12:14:11 +0000

    Login screen basics

--- src/main/java/com/qa/James/GUI/GUIApp.scala
+++ src/main/java/com/qa/James/GUI/GUIApp.scala
@@ -1,64 +1,51 @@
 package com.qa.James.GUI
 
+import com.qa.James.entities.Role
 import scalafx.application.JFXApp
-import scalafx.application.JFXApp.PrimaryStage
+import scalafx.Includes._
+import scalafx.beans.property.StringProperty
 import scalafx.collections.ObservableBuffer
 import scalafx.scene.Scene
+import scalafx.scene.control.Button
+import scalafx.scene.control.TableColumn
 import scalafx.scene.control.TableColumn._
-import scalafx.scene.control.{TableCell, TableColumn, TableView}
-import scalafx.scene.paint.Color
-import scalafx.scene.shape.Circle
-import com.qa.James.entities.Role
-import scalafx.beans.property.StringProperty
-import scalafx.beans.property.IntegerProperty
+import scalafx.scene.control.TableView
 import scalafx.scene.layout.BorderPane
 import scalafx.scene.layout.GridPane
-import scalafx.scene.layout.VBox
+import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.layout.Priority
-import scalafx.scene.control.TableView.ResizeFeatures
-import scalafx.scene.layout.AnchorPane
-import javafx.scene.control.Button
-import scalafx.scene.text.Text
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color
+import scalafx.scene.layout.Background
+import scalafx.scene.layout.BackgroundFill
+import scalafx.scene.layout.CornerRadii
+import scalafx.geometry.Insets
+import scalafx.event.ActionEvent
+import scalafx.application.Platform
+import scalafx.scene.control.Label
+import scalafx.scene.control.TextField
+import scalafx.scene.control.PasswordField
 
 object GUIApp extends JFXApp {
-  
-   val characters = ObservableBuffer[Role](
-       new Role (1, "Operative"),
-       new Role (2, "Manager"),
-       new Role (3, "CEO")
-   )
+ 
   stage=new PrimaryStage {
-    title = "TableView"
+    title = "Login"
     scene = new Scene {
-      content = new AnchorPane{ 
-       children=( new TableView[Role](characters) {
-          columns ++= List(
-            new TableColumn[Role, Int] {
-              text = "Role ID"
-              cellValueFactory = {_.value._idRole}
-              resizable_=(true)
-              hgrow_= (Priority.Always)
-              vgrow_= (Priority.Always)
-            },
-            new TableColumn[Role, String] {
-              text = "Role Title"
-              cellValueFactory = {_.value._roleName}
-              resizable_=(true)
-              hgrow_= (Priority.Always)
-              vgrow_= (Priority.Always)
-            }
-          )
-          columnResizePolicy = TableView.ConstrainedResizePolicy
-          resizable_=(true)
-          hgrow_= (Priority.Always)
-          vgrow_= (Priority.Always)
-        }
-        )
-        resizable_=(true)
-        hgrow_= (Priority.Always)
-        vgrow_= (Priority.Always)
+      content = new BorderPane{
+        center_=(new GridPane{
+          padding = Insets(20)
+          hgap = 20
+          vgap = 20
+          add(new Label("Enter Username:"){}, 0,0)
+          add(new TextField{}, 1,0)
+          add(new Label("Enter Password:"){}, 0,1)
+          add(new PasswordField{}, 1,1)
+        })
+        bottom_=(new GridPane{
+          add(new Button("Login"){}, 0,0)
+          add(new Button("Quit"){}, 1,0)
+        })
       }
     }
-    
   }
 }
