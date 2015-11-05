336c769cc1bfadaf931eac93e0c98a0cdaff4947
jameshforster <jamezx@hotmail.co.uk>
2015-11-05 01:48:55 +0000

    GUI resize failed attempt

--- src/main/java/com/qa/James/GUI/GUIApp.scala
+++ src/main/java/com/qa/James/GUI/GUIApp.scala
@@ -12,38 +12,53 @@ import com.qa.James.entities.Role
 import scalafx.beans.property.StringProperty
 import scalafx.beans.property.IntegerProperty
 import scalafx.scene.layout.BorderPane
+import scalafx.scene.layout.GridPane
+import scalafx.scene.layout.VBox
+import scalafx.scene.layout.Priority
+import scalafx.scene.control.TableView.ResizeFeatures
+import scalafx.scene.layout.AnchorPane
+import javafx.scene.control.Button
+import scalafx.scene.text.Text
 
-
-
-/**
- * @author jforster
- */
-object GUIApp extends JFXApp{
+object GUIApp extends JFXApp {
   
    val characters = ObservableBuffer[Role](
        new Role (1, "Operative"),
        new Role (2, "Manager"),
        new Role (3, "CEO")
    )
-  stage = new PrimaryStage {
+  stage=new PrimaryStage {
     title = "TableView"
     scene = new Scene {
-      content = new BorderPane {
-        center_=( new TableView[Role](characters) {
+      content = new AnchorPane{ 
+       children=( new TableView[Role](characters) {
           columns ++= List(
             new TableColumn[Role, Int] {
               text = "Role ID"
               cellValueFactory = {_.value._idRole}
+              resizable_=(true)
+              hgrow_= (Priority.Always)
+              vgrow_= (Priority.Always)
             },
             new TableColumn[Role, String] {
               text = "Role Title"
               cellValueFactory = {_.value._roleName}
+              resizable_=(true)
+              hgrow_= (Priority.Always)
+              vgrow_= (Priority.Always)
             }
           )
           columnResizePolicy = TableView.ConstrainedResizePolicy
+          resizable_=(true)
+          hgrow_= (Priority.Always)
+          vgrow_= (Priority.Always)
         }
         )
+        resizable_=(true)
+        hgrow_= (Priority.Always)
+        vgrow_= (Priority.Always)
       }
     }
+    
   }
 }
