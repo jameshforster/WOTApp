e500d4f565b789310be5e75237d789fb8f0995b8
jforster <jforster>
2015-11-13 17:30:01 +0000

    Added more Scala tests

--- src/test/java/com/qa/James/loader/CustomerOrderLoaderTest.scala
+++ src/test/java/com/qa/James/loader/CustomerOrderLoaderTest.scala
@@ -1,6 +1,7 @@
 package com.qa.James.loader
 
 import org.scalatest.FlatSpec
+import com.qa.James.entities.CustomerOrder
 
 /**
  * @author jforster
@@ -46,6 +47,35 @@ class CustomerOrderLoaderTest extends FlatSpec{
     "Array" should "not be empty" in {
       val cOLoader = new CustomerOrderLoader[Unit]
       assert(!cOLoader.createCustomerOrderEntities(JDBCConnector.executeSQL(JDBCConnector.querySQL, "SELECT customerorder.*, customerorderstatus.*, employee.*, customer.*, employeeUser.*, customerUser.*, role.* FROM customerorder LEFT JOIN customerorderstatus ON customerorder.idCustomerOrderStatus = customerorderstatus.idCustomerOrderStatus LEFT JOIN employee ON customerorder.idEmployee = employee.idEmployee LEFT JOIN customer ON customerorder.idCustomer = customer.idUser LEFT JOIN user AS employeeUser ON employee.idEmployee = employeeUser.idUser LEFT JOIN user AS customerUser ON customer.idUser = customerUser.idUser LEFT JOIN role ON role.idRole = employee.role_idRole"), null).isEmpty)
+    }
+    
+    "Array" should "contain one object when queried by ID" in {
+      val cOLoader = new CustomerOrderLoader[Int]
+      assert(cOLoader.createCustomerOrderEntities(JDBCConnector.executeSQL(JDBCConnector.querySQL, "SELECT customerorder.*, customerorderstatus.*, employee.*, customer.*, employeeUser.*, customerUser.*, role.* FROM customerorder LEFT JOIN customerorderstatus ON customerorder.idCustomerOrderStatus = customerorderstatus.idCustomerOrderStatus LEFT JOIN employee ON customerorder.idEmployee = employee.idEmployee LEFT JOIN customer ON customerorder.idCustomer = customer.idUser LEFT JOIN user AS employeeUser ON employee.idEmployee = employeeUser.idUser LEFT JOIN user AS customerUser ON customer.idUser = customerUser.idUser LEFT JOIN role ON role.idRole = employee.role_idRole WHERE customerorder.idCustomerOrder = 1"), null).length == 1)
+    }
+    
+    "Array" should "be null when queried by non existent ID" in {
+      val cOLoader = new CustomerOrderLoader[Int]
+      assert(cOLoader.createCustomerOrderEntities(JDBCConnector.executeSQL(JDBCConnector.querySQL, "SELECT customerorder.*, customerorderstatus.*, employee.*, customer.*, employeeUser.*, customerUser.*, role.* FROM customerorder LEFT JOIN customerorderstatus ON customerorder.idCustomerOrderStatus = customerorderstatus.idCustomerOrderStatus LEFT JOIN employee ON customerorder.idEmployee = employee.idEmployee LEFT JOIN customer ON customerorder.idCustomer = customer.idUser LEFT JOIN user AS employeeUser ON employee.idEmployee = employeeUser.idUser LEFT JOIN user AS customerUser ON customer.idUser = customerUser.idUser LEFT JOIN role ON role.idRole = employee.role_idRole WHERE customerorder.idCustomerOrder = 0"), null) == null)
+    }
+    
+    "Array" should "contain Customer Orders" in {
+      val cOLoader = new CustomerOrderLoader[Unit]
+      assert(cOLoader.createCustomerOrderEntities(JDBCConnector.executeSQL(JDBCConnector.querySQL, "SELECT customerorder.*, customerorderstatus.*, employee.*, customer.*, employeeUser.*, customerUser.*, role.* FROM customerorder LEFT JOIN customerorderstatus ON customerorder.idCustomerOrderStatus = customerorderstatus.idCustomerOrderStatus LEFT JOIN employee ON customerorder.idEmployee = employee.idEmployee LEFT JOIN customer ON customerorder.idCustomer = customer.idUser LEFT JOIN user AS employeeUser ON employee.idEmployee = employeeUser.idUser LEFT JOIN user AS customerUser ON customer.idUser = customerUser.idUser LEFT JOIN role ON role.idRole = employee.role_idRole"), null).head.isInstanceOf[CustomerOrder])      
+    }
+    
+    "Array" should "contain non null customer orders" in {
+       val cOLoader = new CustomerOrderLoader[Unit]
+       assert(cOLoader.createCustomerOrderEntities(JDBCConnector.executeSQL(JDBCConnector.querySQL, "SELECT customerorder.*, customerorderstatus.*, employee.*, customer.*, employeeUser.*, customerUser.*, role.* FROM customerorder LEFT JOIN customerorderstatus ON customerorder.idCustomerOrderStatus = customerorderstatus.idCustomerOrderStatus LEFT JOIN employee ON customerorder.idEmployee = employee.idEmployee LEFT JOIN customer ON customerorder.idCustomer = customer.idUser LEFT JOIN user AS employeeUser ON employee.idEmployee = employeeUser.idUser LEFT JOIN user AS customerUser ON customer.idUser = customerUser.idUser LEFT JOIN role ON role.idRole = employee.role_idRole"), null).head != null)
+    }
+  }
+  
+  //Produces same array as createCustomerOrderEntities
+  queryCustomerOrdersTest
+  def queryCustomerOrdersTest {
+    "Final Array" should "not be null" in {
+      val cOLoader = new CustomerOrderLoader[Unit]
+      assert(cOLoader.queryCustomerOrders(cOLoader.createQueryAllCustomerOrders, ()) != null)
     }
   }
 }
